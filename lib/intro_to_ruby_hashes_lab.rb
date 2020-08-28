4425166643cf6cb3c406a51c2a7a8525a4703872
Marisa You <marisa.you@gmail.com>
2020-08-28 22:23:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,17 @@
 def new_hash
-  # return an empty hash
+  return {};
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  return {
+    name: "Marisa"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return {
+    :name => 'Grace Hopper'
+  }
 end
 
 
